5fb4771f78e3f9b5253420ca412c4ad95328f6d4
Gaurav Jawalkar <gaurav@neriticindustries.in>
2025-03-11 16:42:06 +0530

    user model upadte

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -9,7 +9,35 @@ const productSchema = new Schema(
         description: {
             type: String,
             required: true
+        },
+        contents: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        discount: {
+            type: Number,
+            required: true
+        },
+        quantity: {
+            type: Number,
+            required: true
+        },
+        reviews: [{
+            type: String,
+        }],
+        images: [{
+            type: String,
+            required: true
+        }],
+        publisher:{
+            type:Schema.Types.ObjectId,
+            ref:"User",
         }
+
     },
     {
         timestamps: true
---
+++ src/models/user.model.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name:{
+        type:String,
+        required:[true,"Name is a required field"],
+        trim:true
+    },
+    email:{
+        type:String,
+        required:[true,"Email is a required field"],
+        trim:true
+    },
+    password:{
+        type:String,
+        required:[true,"Password is a required field"],
+    },
+    avatar:{
+        type:String,
+        default:"",
+    },
+    refreshToken:{
+        type:String,
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", userSchema);
