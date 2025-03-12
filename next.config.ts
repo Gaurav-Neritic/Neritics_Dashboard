e8e1c7848d707533a5b725ce17b8df28ad4a057b
GauravJawalkar <gauravjawalkar8@gmail.com>
2025-03-13 01:59:36 +0530

    sorted the git conflicts

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-<<<<<<< HEAD
-=======
-  experimental: {
-    viewTransition: true
-  }
->>>>>>> fb4571e (start push in gaurav-home branch)
+
 };
 
 export default nextConfig;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,17 +21,9 @@ const SidebarNav = () => {
             {/* logo Dashboard */}
             <div className="flex items-center justify-center py-3 ">
                 <Link
-<<<<<<< HEAD
                     href={"/"} className="flex items-center justify-center uppercase font-semibold text-lg text-green-600" >
                     <Image src={"/holi logo_12.png"} width={120} height={120} alt="logo" />
                     {/* <span className="text-cyan-600">Neri</span>tic's */}
-=======
-                    href={"/"}
-                    className="flex items-center justify-center uppercase font-semibold text-lg text-green-600"
-                >
-                    <Image src={"/mini_Logo.png"} width={50} height={50} alt="logo" />
-                    <span className="text-cyan-600">Neri</span>tic's
->>>>>>> fb4571e (start push in gaurav-home branch)
                 </Link>
             </div>
 
@@ -39,12 +31,7 @@ const SidebarNav = () => {
             <nav className="py-5">
                 <Link
                     href={"/"}
-<<<<<<< HEAD
                     className="w-full bg-white border border-gray-200 rounded p-2 my-2 flex items-center justify-between gap-2 hover:bg-gray-200 transition-all ease-linear duration-200">
-=======
-                    className="w-full bg-white border border-gray-200 rounded p-2 my-2 flex items-center justify-between gap-2 hover:bg-gray-200 transition-all ease-linear duration-200"
-                >
->>>>>>> fb4571e (start push in gaurav-home branch)
                     <span className="flex gap-3">
                         <House />
                         Home
@@ -53,12 +40,7 @@ const SidebarNav = () => {
 
                 <Link
                     href={"/addProdduct"}
-<<<<<<< HEAD
                     className="w-full bg-teal-100 border border-gray-200 rounded p-2 my-2 flex items-start justify-between gap-2 hover:bg-teal-100 hover:text-teal-600 transition-all ease-linear duration-200">
-=======
-                    className="w-full bg-teal-100 border border-gray-200 rounded p-2 my-2 flex items-start justify-between gap-2 hover:bg-teal-100 hover:text-teal-600 transition-all ease-linear duration-200"
-                >
->>>>>>> fb4571e (start push in gaurav-home branch)
                     <span className="flex gap-3">
                         <SquarePlus className="text-teal-500" />
                         Add Product
@@ -67,12 +49,7 @@ const SidebarNav = () => {
 
                 <Link
                     href={"/orders"}
-<<<<<<< HEAD
                     className="w-full bg-yellow-100 border border-gray-200 rounded p-2 my-2 flex items-start justify-between gap-2 hover:bg-yellow-200 hover:text-yellow-600 transition-all ease-linear duration-200">
-=======
-                    className="w-full bg-yellow-100 border border-gray-200 rounded p-2 my-2 flex items-start justify-between gap-2 hover:bg-yellow-200 hover:text-yellow-600 transition-all ease-linear duration-200"
-                >
->>>>>>> fb4571e (start push in gaurav-home branch)
                     <span className="flex gap-3">
                         <PackagePlus className="text-yellow-500" />
                         Orders
@@ -81,12 +58,7 @@ const SidebarNav = () => {
 
                 <Link
                     href={"/queries"}
-<<<<<<< HEAD
                     className="w-full bg-indigo-100 border border-gray-200 rounded p-2 my-2 flex items-start justify-between gap-2 hover:bg-indigo-200 hover:text-indigo-600 transition-all ease-linear duration-200">
-=======
-                    className="w-full bg-indigo-100 border border-gray-200 rounded p-2 my-2 flex items-start justify-between gap-2 hover:bg-indigo-200 hover:text-indigo-600 transition-all ease-linear duration-200"
-                >
->>>>>>> fb4571e (start push in gaurav-home branch)
                     <span className="flex gap-3">
                         <MessageCircleQuestion className="text-indigo-500" />
                         Queries
@@ -95,12 +67,7 @@ const SidebarNav = () => {
 
                 <Link
                     href={"/sales"}
-<<<<<<< HEAD
                     className="w-full bg-green-100 border border-gray-200 rounded p-2 my-2 flex items-start justify-between gap-2 hover:bg-green-200 hover:text-green-600 transition-all ease-linear duration-200">
-=======
-                    className="w-full bg-green-100 border border-gray-200 rounded p-2 my-2 flex items-start justify-between gap-2 hover:bg-green-200 hover:text-green-600 transition-all ease-linear duration-200"
-                >
->>>>>>> fb4571e (start push in gaurav-home branch)
                     <span className="flex gap-3">
                         <BadgeIndianRupee className="text-green-500" />
                         Sales
@@ -109,12 +76,7 @@ const SidebarNav = () => {
 
                 <Link
                     href={"/sales"}
-<<<<<<< HEAD
                     className="w-full bg-fuchsia-100 border border-gray-200 rounded p-2 my-2 flex items-start justify-between gap-2 hover:bg-fuchsia-200 hover:text-fuchsia-600 transition-all ease-linear duration-200">
-=======
-                    className="w-full bg-fuchsia-100 border border-gray-200 rounded p-2 my-2 flex items-start justify-between gap-2 hover:bg-fuchsia-200 hover:text-fuchsia-600 transition-all ease-linear duration-200"
-                >
->>>>>>> fb4571e (start push in gaurav-home branch)
                     <span className="flex gap-3">
                         <ChartPie className="text-fuchsia-500" />
                         Reports
@@ -123,12 +85,7 @@ const SidebarNav = () => {
 
                 <Link
                     href={"/stocks"}
-<<<<<<< HEAD
                     className="w-full bg-blue-100 border border-gray-200 rounded p-2 my-2 flex items-start justify-between gap-2 hover:bg-blue-200 hover:text-blue-600 transition-all ease-linear duration-200">
-=======
-                    className="w-full bg-blue-100 border border-gray-200 rounded p-2 my-2 flex items-start justify-between gap-2 hover:bg-blue-200 hover:text-blue-600 transition-all ease-linear duration-200"
-                >
->>>>>>> fb4571e (start push in gaurav-home branch)
                     <span className="flex gap-3">
                         <Blocks className="text-blue-500" />
                         Stocks
@@ -137,12 +94,7 @@ const SidebarNav = () => {
 
                 <Link
                     href={"/report"}
-<<<<<<< HEAD
                     className="w-full bg-red-100 border border-gray-200 rounded p-2 my-2 flex items-start justify-between gap-2 hover:bg-red-200 hover:text-red-500 transition-all ease-linear duration-200">
-=======
-                    className="w-full bg-red-100 border border-gray-200 rounded p-2 my-2 flex items-start justify-between gap-2 hover:bg-red-200 hover:text-red-500 transition-all ease-linear duration-200"
-                >
->>>>>>> fb4571e (start push in gaurav-home branch)
                     <span className="flex gap-3">
                         <Bug className="text-red-500" />
                         Report Bug
@@ -151,12 +103,7 @@ const SidebarNav = () => {
 
                 <Link
                     href={"/settings"}
-<<<<<<< HEAD
                     className="w-full bg-gray-100 border border-gray-200 rounded p-2 my-2 flex items-start justify-between gap-2 hover:bg-gray-200 hover:text-gray-700 transition-all ease-linear duration-200">
-=======
-                    className="w-full bg-gray-100 border border-gray-200 rounded p-2 my-2 flex items-start justify-between gap-2 hover:bg-gray-200 hover:text-gray-700 transition-all ease-linear duration-200"
-                >
->>>>>>> fb4571e (start push in gaurav-home branch)
                     <span className="flex gap-3">
                         <Settings className="text-gray-700" />
                         Settings
