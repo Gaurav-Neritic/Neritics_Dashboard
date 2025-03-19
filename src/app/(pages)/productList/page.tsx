3315832ce5eccc5ca7429a2b285629329c690f1c
Sanket-Neritic <sanket@neriticindustries.in>
2025-03-19 18:19:11 +0530

    Products List last Update

--- src/app/(pages)/productList/page.tsx
+++ src/app/(pages)/productList/page.tsx
@@ -40,9 +40,9 @@ const ProductList = () => {
       {/* Products List */}
 
       <div className="py-5">
-        <div className="p-1 h-screen border border-gray-300 dark:border-neutral-700 rounded ">
-          <div className="m-5  border border-gray-300 dark:border-neutral-700 rounded">
-            <div className=" py-3 flex justify-around items-center ">
+        <div className="p-1  border border-gray-300 dark:border-neutral-700 rounded ">
+          <div className=" bg-neutral-100/10 dark:bg-neutral-500/10  m-5  border border-gray-300 dark:border-neutral-700 rounded">
+            <div className=" py-3 grid grid-cols-6 place-items-center gap-3 ">
               <h1>Products Id</h1>
               <h1>Products Name</h1>
               <h1>Price</h1>
@@ -53,17 +53,17 @@ const ProductList = () => {
             <hr className=" my-1 border-gray-300 dark:border-neutral-500" />
             <div>
               {/* Product Item */}
-              <div className="py-3 flex justify-around items-center border-b border-gray-200 dark:border-neutral-600 text-gray-500 dark:text-gray-50">
+              <div className="py-3 grid grid-cols-6 place-items-center gap-3 border-b border-gray-200 dark:border-neutral-600 text-gray-500 dark:text-gray-50 ">
                 <h1>Product 1</h1>
                 <h1>Product 1 Name</h1>
                 <h1>$100</h1>
                 <h1>Category 1</h1>
                 <h1>100</h1>
-                <button className="text-red-400 hover:text-red-500">
-                  <Trash2 className="text-sm" />
+                <button className="text-red-400 hover:text-red-500 grid place-items-center ">
+                  <Trash2 className="text-s " />
                 </button>
               </div>
-              <div className="py-3 flex justify-around items-center border-b border-gray-200 dark:border-neutral-600 text-gray-500 dark:text-gray-50">
+              <div className="py-3 grid grid-cols-6 place-items-center gap-3 border-b border-gray-200 dark:border-neutral-600 text-gray-500 dark:text-gray-50">
                 <h1>Product 2</h1>
                 <h1>Product 2 Name</h1>
                 <h1>$100</h1>
@@ -73,7 +73,7 @@ const ProductList = () => {
                   <Trash2 className="text-sm" />
                 </button>
               </div>
-              <div className="py-3 flex justify-around items-center border-b border-gray-200 dark:border-neutral-600 text-gray-500 dark:text-gray-50">
+              <div className="py-3 grid grid-cols-6 place-items-center gap-3 border-b border-gray-200 dark:border-neutral-600 text-gray-500 dark:text-gray-50">
                 <h1>Product 3</h1>
                 <h1>Product 3 Name</h1>
                 <h1>$100</h1>
@@ -83,7 +83,7 @@ const ProductList = () => {
                   <Trash2 className="text-sm" />
                 </button>
               </div>
-              <div className="py-3 flex justify-around items-center border-b border-gray-200 dark:border-neutral-600 text-gray-500 dark:text-gray-50">
+              <div className="py-3 grid grid-cols-6 place-items-center gap-3 border-b border-gray-200 dark:border-neutral-600 text-gray-500 dark:text-gray-50">
                 <h1>Product 4</h1>
                 <h1>Product 4 Name</h1>
                 <h1>$100</h1>
@@ -93,7 +93,7 @@ const ProductList = () => {
                   <Trash2 className="text-sm" />
                 </button>
               </div>
-              <div className="py-3 flex justify-around items-center border-b border-gray-200 dark:border-neutral-600 text-gray-500 dark:text-gray-50">
+              <div className="py-3 grid grid-cols-6 place-items-center gap-3 border-b border-gray-200 dark:border-neutral-600 text-gray-500 dark:text-gray-50">
                 <h1>Product 5</h1>
                 <h1>Product 5 Name</h1>
                 <h1>$100</h1>
@@ -103,7 +103,7 @@ const ProductList = () => {
                   <Trash2 className="text-sm" />
                 </button>
               </div>
-              <div className="py-3 flex justify-around items-center border-b border-gray-200 dark:border-neutral-600 text-gray-500 dark:text-gray-50">
+              <div className="py-3 grid grid-cols-6 place-items-center gap-3 border-b border-gray-200 dark:border-neutral-600 text-gray-500 dark:text-gray-50">
                 <h1>Product 6</h1>
                 <h1>Product 6 Name</h1>
                 <h1>$100</h1>
@@ -113,7 +113,7 @@ const ProductList = () => {
                   <Trash2 className="text-sm" />
                 </button>
               </div>
-              <div className="py-3 flex justify-around items-center border-b border-gray-200 dark:border-neutral-600 text-gray-500 dark:text-gray-50">
+              <div className="py-3 grid grid-cols-6 place-items-center gap-3 border-b border-gray-200 dark:border-neutral-600 text-gray-500 dark:text-gray-50">
                 <h1>Product 6</h1>
                 <h1>Product 6 Name</h1>
                 <h1>$100</h1>
@@ -123,7 +123,7 @@ const ProductList = () => {
                   <Trash2 className="text-sm" />
                 </button>
               </div>
-              <div className="py-3 flex justify-around items-center border-b border-gray-200 dark:border-neutral-600 text-gray-500 dark:text-gray-50">
+              <div className="py-3 grid grid-cols-6 place-items-center gap-3 border-b border-gray-200 dark:border-neutral-600 text-gray-500 dark:text-gray-50">
                 <h1>Product 7</h1>
                 <h1>Product 7 Name</h1>
                 <h1>$100</h1>
@@ -133,7 +133,7 @@ const ProductList = () => {
                   <Trash2 className="text-sm" />
                 </button>
               </div>
-              <div className="py-3 flex justify-around items-center border-b border-gray-200 dark:border-neutral-600 text-gray-500 dark:text-gray-50">
+              <div className="py-3 grid grid-cols-6 place-items-center gap-3 border-b border-gray-200 dark:border-neutral-600 text-gray-500 dark:text-gray-50">
                 <h1>Product 8</h1>
                 <h1>Product 8 Name</h1>
                 <h1>$100</h1>
@@ -143,7 +143,7 @@ const ProductList = () => {
                   <Trash2 className="text-sm" />
                 </button>
               </div>
-              <div className="py-3 flex justify-around items-center border-b border-gray-200 dark:border-neutral-600 text-gray-500 dark:text-gray-50">
+              <div className="py-3 grid grid-cols-6 place-items-center gap-3 border-b border-gray-200 dark:border-neutral-600 text-gray-500 dark:text-gray-50">
                 <h1>Product 9</h1>
                 <h1>Product 9 Name</h1>
                 <h1>$100</h1>
@@ -153,7 +153,7 @@ const ProductList = () => {
                   <Trash2 className="text-sm" />
                 </button>
               </div>
-              <div className="py-3 flex justify-around items-center border-b border-gray-200 dark:border-neutral-600 text-gray-500 dark:text-gray-50">
+              <div className="py-3 grid grid-cols-6 place-items-center gap-3 border-b border-gray-200 dark:border-neutral-600 text-gray-500 dark:text-gray-50">
                 <h1>Product 10</h1>
                 <h1>Product 10 Name</h1>
                 <h1>$100</h1>
