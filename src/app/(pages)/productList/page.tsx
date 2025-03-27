5545508750ce9214098a6d1c20cc644c45a2d9ea
Sanket-Neritic <sanket@neriticindustries.in>
2025-03-27 12:40:00 +0530

    ProductList border-color

--- src/app/(pages)/productList/page.tsx
+++ src/app/(pages)/productList/page.tsx
@@ -84,27 +84,27 @@ const ProductList = () => {
       <div className="border px-4 py-2 rounded border-lightBorder dark:border-darkBorder ">
         <div className="flex items-center justify-start gap-3">
           <SlidersHorizontal />
-          <select className="border border-gray-400 px-3 py-1 rounded outline-none">
+          <select className="border  border-lightBorder dark:border-darkBorder px-3 py-1 rounded outline-none">
             <option className="dark:bg-neutral-600">Filter One</option>
             <option className="dark:text-black">Option 2</option>
             <option className="dark:text-black">Option 3</option>
             <option className="dark:text-black">Option 4</option>
           </select>
-          <select className="border border-gray-400 px-3 py-1 rounded outline-none">
+          <select className="border  border-lightBorder dark:border-darkBorder px-3 py-1 rounded outline-none">
             <option className="dark:bg-neutral-600">Type</option>
             <option className="dark:text-black">Type 2</option>
             <option className="dark:text-black">Type 3</option>
             <option className="dark:text-black">Type 4</option>
           </select>
 
-          <select className="border border-gray-400 px-3 py-1 rounded outline-none">
+          <select className="border  border-lightBorder dark:border-darkBorder px-3 py-1 rounded outline-none">
             <option className="dark:bg-neutral-600">Range</option>
             <option className="dark:text-black">From 1 to 2</option>
             <option className="dark:text-black">From 2 to 3</option>
             <option className="dark:text-black">From 3 to 4</option>
           </select>
 
-          <select className="border border-gray-400 px-3 py-1 rounded outline-none">
+          <select className="border  border-lightBorder dark:border-darkBorder px-3 py-1 rounded outline-none">
             <option className="dark:bg-neutral-600">Time</option>
             <option className="dark:text-black">Recently</option>
             <option className="dark:text-black">Frequently</option>
@@ -115,7 +115,7 @@ const ProductList = () => {
             onChange={(e) => {
               setFilter(e.target.value);
             }}
-            className="border border-gray-400 px-3 py-1 rounded outline-none"
+            className="border  border-lightBorder dark:border-darkBorder px-3 py-1 rounded outline-none"
           >
             <option className="dark:bg-neutral-600">All</option>
             <option className="dark:text-black">Listed</option>
