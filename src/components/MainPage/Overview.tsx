ce39d7831f11f6b51b83464df99625bb9307b9a2
Gaurav Jawalkar <gaurav@neriticindustries.in>
2025-04-25 15:15:02 +0530

    analytics data changed for frontend charts [pie chart]

--- src/components/MainPage/Overview.tsx
+++ src/components/MainPage/Overview.tsx
@@ -53,18 +53,17 @@ const options = {
 };
 
 export const data = {
-  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  labels: ["Red", "Blue", "Yellow", "Green", "Purple"],
   datasets: [
     {
       label: "Votes",
-      data: [12, 19, 3, 5, 2, 3],
+      data: [12, 19, 3, 5, 2],
       backgroundColor: [
         "rgba(255, 99, 132, 0.4)",
         "rgba(54, 162, 235, 0.4)",
         "rgba(255, 206, 86, 0.4)",
         "rgba(75, 192, 192, 0.4)",
         "rgba(153, 102, 255, 0.4)",
-        "rgba(255, 159, 64, 0.4)",
       ],
       borderColor: [
         "rgba(255, 99, 132, 1)",
