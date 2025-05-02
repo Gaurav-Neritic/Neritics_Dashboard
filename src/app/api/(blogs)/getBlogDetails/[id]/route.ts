d4a177bb56ca8e6f218fe1b6c692c1c56b259380
Gaurav Jawalkar <gaurav@neriticindustries.in>
2025-05-02 12:03:04 +0530

    done debugging

--- src/app/api/(blogs)/getBlogDetails/[id]/route.ts
+++ src/app/api/(blogs)/getBlogDetails/[id]/route.ts
@@ -21,7 +21,7 @@ export async function GET(_: NextRequest, params: { params: Promise<{ id: string
     }
 
     try {
-        const { id } = await params;
+        const { id } = await params.params;
 
         console.log(id);
 
