b8095a7ae2327c6427a028d2a9b2f6508bca4619
Gaurav Jawalkar <gaurav@neriticindustries.in>
2025-05-08 11:11:05 +0530

    final opt

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [{ hostname: 'res.cloudinary.com' }],
-  }
-
+  },
+  env: {
+    NEXT_FONT_IGNORE_LIGHTNINGCSS: 'true', // <-- Force at build time
+  },
 };
 
 export default nextConfig;
